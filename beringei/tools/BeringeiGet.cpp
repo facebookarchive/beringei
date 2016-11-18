623645848b56844277e0be558f352687a2790bcc
Brian Smith <bcsmith@fb.com>
2016-11-18 15:05:49 -0800

    Add get and put tools for beringei
    Summary:
    These are simple tools to put and get data out of beringei. Not meant for prod,
    but more as examples of how to interact with the service.
    
    Reviewed By: jteller
    
    Differential Revision: D4166524
    
    fbshipit-source-id: 8aed85f11b6da5e8653781c20a8c2256e69babc2

---
+++ beringei/tools/BeringeiGet.cpp
@@ -0,0 +1,120 @@
+/**
+* Copyright (c) 2016-present, Facebook, Inc.
+* All rights reserved.
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+#include "beringei/client/BeringeiClient.h"
+#include "beringei/plugins/BeringeiConfigurationAdapter.h"
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <set>
+#include <string>
+
+#include <folly/Conv.h>
+
+using namespace facebook;
+
+template <class T>
+std::chrono::seconds to_epoch(T tp) {
+  return std::chrono::duration_cast<std::chrono::seconds>(
+      tp.time_since_epoch());
+}
+
+DECLARE_string(beringei_configuration_path);
+DEFINE_int32(
+    shard_id,
+    -1,
+    "Override the calculated shard_id. -1 to use the calculated shard.");
+DEFINE_int64(
+    start_time,
+    0,
+    "Unix timestamp of the start time to query. 0 means 'now'.");
+DEFINE_int64(
+    end_time,
+    0,
+    "Unix timestamp of the end time to query. Must be > --start_time.");
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::SetUsageMessage("[<options>] <key>");
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  auto beringeiConfig =
+      std::make_shared<gorilla::BeringeiConfigurationAdapter>(true);
+  auto beringeiClient =
+      std::make_shared<gorilla::BeringeiClient>(beringeiConfig, 1, 0, false);
+
+  if (FLAGS_start_time == 0) {
+    FLAGS_start_time =
+        to_epoch(std::chrono::system_clock::now() - std::chrono::hours(3))
+            .count();
+  }
+  if (FLAGS_end_time == 0) {
+    FLAGS_end_time = to_epoch(std::chrono::system_clock::now()).count();
+  }
+  LOG(INFO) << "Start time: " << FLAGS_start_time
+            << "; End time: " << FLAGS_end_time;
+
+  if (argc < 2) {
+    google::ShowUsageWithFlagsRestrict(argv[0], "beringei");
+    return 1;
+  }
+
+  int shardCount = beringeiClient->getNumShards();
+  LOG(INFO) << "Config knows about these read services: ";
+  for (const auto& rservice : beringeiConfig->getReadServices()) {
+    LOG(INFO) << "  " << rservice;
+  }
+  LOG(INFO) << "Beringei has " << shardCount << " shards.";
+
+  if (shardCount == 0) {
+    LOG(FATAL) << "Shard count can't be zero, though.";
+  }
+
+  LOG(INFO) << "Now: " << to_epoch(std::chrono::system_clock::now()).count();
+
+  std::string keyName = std::string(argv[1]);
+
+  if (FLAGS_shard_id == -1) {
+    FLAGS_shard_id = std::hash<std::string>()(keyName) % shardCount;
+  }
+
+  LOG(INFO) << "Key is in shard_id: " << FLAGS_shard_id;
+
+  gorilla::GetShardDataBucketResult shardResult;
+  beringeiClient->getShardDataBucket(
+      FLAGS_start_time - 1201,
+      FLAGS_end_time - 599,
+      FLAGS_shard_id,
+      0,
+      10000,
+      shardResult);
+
+  LOG(INFO) << "Get whole shard stats:";
+  LOG(INFO) << "Request status: " << (int)shardResult.status;
+  LOG(INFO) << "Keys: " << folly::join(", ", shardResult.keys);
+
+  gorilla::GetDataRequest request;
+  request.keys.emplace_back();
+  request.keys.back().key = keyName;
+  request.keys.back().shardId = FLAGS_shard_id;
+  request.begin = FLAGS_start_time;
+  request.end = FLAGS_end_time;
+
+  gorilla::GorillaResultVector result;
+  beringeiClient->get(request, result);
+
+  for (const auto& keyData : result) {
+    const auto& keyName = keyData.first.key;
+    for (const auto& timeValue : keyData.second) {
+      std::cout << keyName << "\t" << timeValue.value << "\t"
+                << timeValue.unixTime << std::endl;
+    }
+  }
+  return 0;
+}
---
+++ beringei/tools/BeringeiPut.cpp
@@ -0,0 +1,75 @@
+/**
+* Copyright (c) 2016-present, Facebook, Inc.
+* All rights reserved.
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+#include "beringei/client/BeringeiClient.h"
+#include "beringei/plugins/BeringeiConfigurationAdapter.h"
+
+#include <chrono>
+#include <memory>
+#include <set>
+#include <string>
+
+#include <folly/Conv.h>
+
+using namespace facebook;
+
+DECLARE_string(beringei_configuration_path);
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::SetUsageMessage("[<options>] <key> <value> [<timestamp>]");
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  auto beringeiConfig =
+      std::make_shared<gorilla::BeringeiConfigurationAdapter>(true);
+  auto beringeiClient =
+      std::make_shared<gorilla::BeringeiClient>(beringeiConfig, 10, 5, true);
+
+  if (argc < 3) {
+    google::ShowUsageWithFlagsRestrict(argv[0], "BeringeiPut.cpp");
+    return 1;
+  }
+
+  int shardCount = beringeiClient->getNumShardsFromWriteClient();
+  LOG(INFO) << "Config knows about these write services: ";
+  for (const auto& wservice : beringeiConfig->getWriteServices()) {
+    LOG(INFO) << "  " << wservice;
+  }
+  LOG(INFO) << "Beringei has " << shardCount << " shards";
+
+  if (shardCount == 0) {
+    LOG(FATAL) << "Shard count can't be zero, though.";
+  }
+
+  std::vector<gorilla::DataPoint> dps;
+  dps.emplace_back();
+  dps.back().key.key = std::string(argv[1]);
+  dps.back().key.shardId =
+      std::hash<std::string>()(dps.back().key.key) % shardCount;
+  dps.back().value.unixTime =
+      std::chrono::duration_cast<std::chrono::seconds>(
+          std::chrono::system_clock::now().time_since_epoch())
+          .count();
+  dps.back().value.value = folly::to<double>(argv[2]);
+
+  LOG(INFO) << "Num pts: " << dps.size();
+
+  if (argc > 3) {
+    dps.back().value.unixTime = folly::to<int64_t>(argv[3]);
+  }
+
+  auto pushedPoints = beringeiClient->putDataPoints(dps);
+
+  if (!pushedPoints) {
+    LOG(ERROR) << "Failed to perform the put!";
+  }
+
+  beringeiClient->flushQueue();
+
+  return 0;
+}
