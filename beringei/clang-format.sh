36d009d01f1dc9cf3c039c0ad45bedb535bcf96a
Scott Franklin <scottf@fb.com>
2016-11-24 17:13:55 +0000

    Review changes.
    Added whitespace to a file and re-ran the script from a different working
    dir to verify it still worked.

--- beringei/clang-format.sh
+++ beringei/clang-format.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIR=$(dirname "$0")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 find "$DIR" -type f -name '*.cpp' -execdir clang-format-3.9 -i {} +
 find "$DIR" -type f -name '*.h' -execdir clang-format-3.9 -i {} +
