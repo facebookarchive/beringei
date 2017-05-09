91aa47a571145fa723ac6418050ac12c42bb0ffd
Scott Franklin <scottf@fb.com>
2017-05-09 12:53:33 -0700

    Improve CMake dependency tree
    Summary:
    - Require gtest headers to be downloaded before building unit tests
    - Turn thrift generation into a proper build target
    Closes https://github.com/facebookincubator/beringei/pull/43
    
    Differential Revision: D4976669
    
    Pulled By: scottfranklin
    
    fbshipit-source-id: 4e020b53051327d7b71d7ecab33de3bafa91acb0

--- beringei/if/build_thrift.sh
+++ beringei/if/build_thrift.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# fail script on any error
+# Fail script on any error.
 set -e
 
-# build every thrift file
-for THRIFT_FILE in $(ls *.thrift); do
+# Build thrift files.
+for THRIFT_FILE in "$@"; do
     echo "Building file: " $THRIFT_FILE
-    PYTHONPATH=/tmp/fbthrift-2016.11.07.00/thrift/.python-local/lib/python  python2 -mthrift_compiler.main --gen cpp2 $THRIFT_FILE -I../..
+    python2 -mthrift_compiler.main --gen cpp2 "$THRIFT_FILE" -I../..
 done
