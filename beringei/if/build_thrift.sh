e0d7772de1211ea9a596814cd91fdfaa4d589f7d
Scott Franklin <scottf@fb.com>
2017-05-09 11:43:52 -0700

    Punctuation

--- beringei/if/build_thrift.sh
+++ beringei/if/build_thrift.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Fail script on any error
+# Fail script on any error.
 set -e
 
-# Build thrift files
+# Build thrift files.
 for THRIFT_FILE in "$@"; do
     echo "Building file: " $THRIFT_FILE
     python2 -mthrift_compiler.main --gen cpp2 "$THRIFT_FILE" -I../..
