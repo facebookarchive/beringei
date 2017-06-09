d59001477d786f9d14e082a504f4af89296f1ee5
Victor Gao <vgao@fb.com>
2017-06-09 11:17:11 -0700

    apply clang-tidy modernize-use-override
    Summary: Use clang-tidy to mechanically add missing `override` and remove redundant `virtual`.
    
    Reviewed By: igorsugak
    
    Differential Revision: D5211868
    
    fbshipit-source-id: 15cec17d39690ffa8072ffeccdf9fedaae1f6839

--- beringei/service/BeringeiServiceHandler.h
+++ beringei/service/BeringeiServiceHandler.h
@@ -41,16 +41,16 @@ class BeringeiServiceHandler : virtual public BeringeiServiceSvIf {
       int port,
       bool adjustTimestamps = true);
 
-  virtual ~BeringeiServiceHandler();
+  ~BeringeiServiceHandler() override;
 
-  virtual void getData(GetDataResult& ret, std::unique_ptr<GetDataRequest> req)
+  void getData(GetDataResult& ret, std::unique_ptr<GetDataRequest> req)
       override;
 
-  virtual void putDataPoints(
+  void putDataPoints(
       PutDataResult& response,
       std::unique_ptr<PutDataRequest> req) override;
 
-  virtual void getShardDataBucket(
+  void getShardDataBucket(
       GetShardDataBucketResult& ret,
       int64_t beginTs,
       int64_t endTs,
@@ -60,7 +60,7 @@ class BeringeiServiceHandler : virtual public BeringeiServiceSvIf {
 
   virtual BucketMap* getShardMap(int64_t shardId);
 
-  virtual void getLastUpdateTimes(
+  void getLastUpdateTimes(
       GetLastUpdateTimesResult& ret,
       std::unique_ptr<GetLastUpdateTimesRequest> req) override;
 
